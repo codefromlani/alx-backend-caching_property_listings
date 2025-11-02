b3d94f63be36e7d88e5552ab6178512109a6c2c0
codefromlani <rodiathammed48@gmail.com>
2025-11-02 11:50:26 +0100

    feat: cache property list view in Redis for 15 minutes

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+# Cache the view for 15 minutes (900 seconds)
+@cache_page(60 * 15)
+def property_list(request):
+    properties = list(Property.objects.values('id', 'title', 'description', 'price', 'location', 'created_at'))
+    return JsonResponse({'results': properties})
